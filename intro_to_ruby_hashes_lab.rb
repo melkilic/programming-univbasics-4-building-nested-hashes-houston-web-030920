d8e1d302ae5f8bbda2034097f922451ed1667991
Melike KILIC <melike180400@gmail.com>
2020-02-17 05:18:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -55,7 +55,7 @@ def monopoly_with_third_tier
     },
       :names => {
         :reading_railroad => {"mortgage_value" => "$100"} ,
-        :pennsylvania_railroad => {},
+        :pennsylvania_railroad => {"mortgage_value" => "$200"},
         :b_and_o_railroad => {},
         :shortline_railroad => {}
         
