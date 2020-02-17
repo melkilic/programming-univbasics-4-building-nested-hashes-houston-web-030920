a631763f77f49df0df2cec83e0d24d871b96751a
Melike KILIC <melike180400@gmail.com>
2020-02-17 05:12:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -43,5 +43,22 @@ def monopoly_with_third_tier
  end
 
  def monopoly_with_fourth_tier
-  
+   :railroads => 
+    { 
+    :pieces => 4,
+    :rent_in_dollars => {
+      :one_piece_owned => 25,
+      :two_pieces_owned => 50,
+      :three_pieces_owned => 100,
+      :four_piece_owned => 200
+    },
+      :names => {
+        :reading_railroad => {} ,
+        :pennsylvania_railroad => {},
+        :b_and_o_railroad => {},
+        :shortline_railroad => {}
+        
+      }
+    }
+  }
  end
